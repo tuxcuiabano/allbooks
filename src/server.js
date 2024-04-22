a2fafd212229170d3039d34ee7d39e640fd68403
Pedro Neto <pedro@tuxcuiabano.com>
2024-04-22 14:19:29 -0400

    Alteradoooo

--- src/server.js
+++ src/server.js
@@ -18,6 +18,6 @@ server.use(/^(?!\/(public|livros|autores|categorias)).*$/, authenticationMiddlew
 server.use(router) //rotas na prota 3000
 
 server.listen(8000, () => {
-  console.log("Bem vindo API")
+  console.log("Bem vindo A")
   console.log("NOSAA API disponível em http://localhost:8000")
 })
