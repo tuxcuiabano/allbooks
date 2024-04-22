3404ec950523007acf9e42dd1bf38134c49eace7
Pedro Neto <pedro@tuxcuiabano.com>
2024-04-22 14:06:43 -0400

    Altera o SERVER JS

--- src/server.js
+++ src/server.js
@@ -18,5 +18,6 @@ server.use(/^(?!\/(public|livros|autores|categorias)).*$/, authenticationMiddlew
 server.use(router) //rotas na prota 3000
 
 server.listen(8000, () => {
-  console.log("API disponível em http://localhost:8000")
+  console.log("Bem vindo API")
+  console.log("NOSAA API disponível em http://localhost:8000")
 })
